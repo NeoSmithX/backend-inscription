acb2d27e88806f90a4e3f1c40a95bc3ec129f978
Neo <NeoSmith1999@protonmail.com>
2023-12-09 12:47:38 +0800

    add request for generating user profile

--- src/my_fn/relay_frontend_side.ts
+++ src/my_fn/relay_frontend_side.ts
@@ -35,7 +35,7 @@ export const fetchTaskFromSql = async () => {
     console.log('fetchTaskFromSql function is working')
     while (true) {
         try {
-            const pythonProcess = spawn('python', ['DB_backend/5_fetchTask.py']);
+            const pythonProcess = spawn('python', ['DB_backend/8_fetchTask.py']);
 
             pythonProcess.stdout.on('data', (data) => {
                 // console.log(`stdout: ${data}`)
@@ -95,7 +95,7 @@ export const fetchTaskFromSql = async () => {
 
 }
 
-export const distributeTask = async () => {
+export const distributeTask = async () => { // this is to distrubute task to the stable-diffusion ai side
     console.log('distributeTask function is working')
     app.post('/fetchTask', async (req: any, res: { json: (arg0: string | Task[]) => void; }) => {
         const tasks = taskGlobal.filter((task: Task) => task.isCompleted == false)
@@ -112,39 +112,39 @@ export const distributeTask = async () => {
 
 }
 
-export const receiveImgFromAiSide = async () => {
-    app.post('/uploadImg', upload.single('image'), (req: any, res: any) => {
-        console.log('!!!!!', req)
-        const taskID = req.body.taskID
-        console.log('Received taskId :', taskID);
+// export const receiveImgFromAiSide = async () => {
+//     app.post('/uploadImg', upload.single('image'), (req: any, res: any) => {
+//         console.log('!!!!!', req)
+//         const taskID = req.body.taskID
+//         console.log('Received taskId :', taskID);
 
-        console.log('Received file:', req.file);
-        console.log('Received body:', req.body); // Assuming 'text' is the key for the string data
+//         console.log('Received file:', req.file);
+//         console.log('Received body:', req.body); // Assuming 'text' is the key for the string data
 
-        res.status(200).json({ message: 'File and text received' });
+//         res.status(200).json({ message: 'File and text received' });
 
 
 
-        const task = taskGlobal.find((task: Task) => task.taskID == req.body.text);
-        if (task) {
-            const tempPath = req.file.path;
-            const targetPath = path.join(__dirname, 'uploads', taskID);
+//         const task = taskGlobal.find((task: Task) => task.taskID == req.body.text);
+//         if (task) {
+//             const tempPath = req.file.path;
+//             const targetPath = path.join(__dirname, 'uploads', taskID);
 
-            // Move and rename the file
-            fs.rename(tempPath, targetPath, (err: any) => {
-                if (err) return res.status(500).json({ message: 'Error saving the file' });
+//             // Move and rename the file
+//             fs.rename(tempPath, targetPath, (err: any) => {
+//                 if (err) return res.status(500).json({ message: 'Error saving the file' });
 
-                res.status(200).json({ message: 'File uploaded successfully' });
-            });
-            const pythonProcess = spawn('python', ['DB_backend/3_submit_task.py', task.taskID, task.imgPath]);
-            task.isCompleted = true;
-        }
+//                 res.status(200).json({ message: 'File uploaded successfully' });
+//             });
+//             const pythonProcess = spawn('python', ['DB_backend/3_submit_task.py', task.taskID, task.imgPath]);
+//             task.isCompleted = true;
+//         }
 
-    })
-    app.listen(configRelay.frontend.port, () => {
-        console.log(`Server running on ` + configRelay.frontend.url + ':' + configRelay.frontend.port);
-    });
-}
+//     })
+//     app.listen(configRelay.frontend.port, () => {
+//         console.log(`Server running on ` + configRelay.frontend.url + ':' + configRelay.frontend.port);
+//     });
+// }
 export const receiveImgFromAiSide_v2 = async () => {
     console.log('receiveImgFromAiSide_v2 function is working')
     app.post('/uploadImg', (req: any, res: any) => {
@@ -160,7 +160,7 @@ export const receiveImgFromAiSide_v2 = async () => {
             const imageBuffer = Buffer.from(image, 'base64');
 
             fs.writeFileSync(task.imgPath, imageBuffer);
-            const pythonProcess = spawn('python', ['DB_backend/3_submit_task.py', task.taskID, task.imgPath]);
+            const pythonProcess = spawn('python', ['DB_backend/6_submit_task.py', task.taskID, task.imgPath]);
             task.isCompleted = true;
         }
 
@@ -229,14 +229,14 @@ export const verifyUserSignature = async () => {
 
 export const generateTaskFromFrontend = async () => {
     console.log('generateTaskFromFrontend function is working')
-    app.post('/generateTaskFromFrontend', (req: any, res: any) => {
+    app.post('/generateTaskFromFrontend', async (req: any, res: any) => {
         console.log('receive generateTaskFromFrontend from '+req.ip)
         console.log( req.body)
         const { userAddress, feature } = req.body;
 
         if (userAddress && feature) {
             try{
-                spawn('python', ['DB_backend/2_create_task.py', userAddress, JSON.stringify(feature) ])
+                spawn('python', ['DB_backend/5_create_task.py', userAddress, JSON.stringify(feature) ])
                 console.log('the nft task has been added into SQL database, and the AI-imgae will be generated soon (absolute path will be added in the next test')
                 res.json('the nft task has been added into SQL database, and the AI-imgae will be generated soon (absolute path will be added in the next test)')
             }catch(e){
@@ -249,7 +249,106 @@ export const generateTaskFromFrontend = async () => {
             res.json('task generation failed, please check the input format')
         }
         
-        // "0xabcdef" "{\"feature1\":\"ff111\", \"feature2\":\"ff12323232\"}"
+    
+
+    })
+
+}
+
+
+export const createUserProfile = async () => {
+    console.log('createUserProfile function is working')
+    app.post('/createUserProfile', async (req: any, res: any) => {
+        console.log('receive createUserProfile from '+req.ip)
+        console.log( req.body)
+        const { accountType, userAddress } = req.body
+
+        if (accountType && userAddress) {
+            const data = {...JSON.parse(req.body),NFTEligible:0}
+            try{
+                spawn('python', ['DB_backend/2_setup_users.py', JSON.stringify(data) ])
+        
+                res.json('success for createUserProfile')
+            }catch(e){
+                console.log(e)
+                res.json('fail for createUserProfile')
+            }
+            
+        }else{
+            console.log(accountType,userAddress)
+            res.json('createUserProfile failed, please check the input format')
+        }
+        
+    
+
+    })
+
+}
+export const checkNFTCode = async () => {   // this will update user profile to NFTEligible ++
+    console.log('checkNFTCode function is working')
+    app.post('/checkNFTCode', async (req: any, res: any) => {
+        console.log('receive checkNFTCode from '+req.ip)
+        console.log( req.body)
+        const { accountType, userAddress, NFTCode } = req.body
+
+        if (accountType && userAddress && NFTCode) {
+            try{
+                const isNFTCodeValid = spawn('python', ['DB_backend/3_1_checkNFTcode.py', NFTCode ])
+
+                if (isNFTCodeValid){
+                  
+                    // spawn('python', ['DB_backend/2_setup_users.py', JSON.stringify(data) ])
+                    res.json('NFT code is valid and the user NFTEligible is updated')
+                }else{
+                    res.json('NFT code is invalid')
+                }
+
+            }catch(e){
+                console.log(e)
+                res.json('fail for checkNFTCode')
+            }
+       
+            
+            
+        }else{
+            console.log(accountType,userAddress)
+            res.json('createUserProfile failed, please check the input format')
+        }
+        
+    
+
+    })
+
+}
+
+
+
+export const getUserAllImg = async () => {
+    const functionName = 'getUserAllImg'
+    console.log(functionName+' function is working')
+    app.post('/'+functionName, async (req: any, res: any) => {
+        console.log('receive' + functionName+' from '+req.ip)
+        console.log( req.body)
+        const { accountType, userAddress } = req.body
+
+        if (accountType && userAddress) {
+           
+            try{
+                const data = spawn('python', ['DB_backend/9_fetchIMG.py', req.body ])
+        
+                res.json(data)
+                
+            }catch(e){
+                console.log(e)
+                res.json('fail for '+functionName)
+            }
+            
+        }else{
+            console.log(accountType,userAddress)
+            res.json(functionName+ ' failed, please check the input format')
+        }
+        
+    
 
     })
 
@@ -258,3 +357,5 @@ export const generateTaskFromFrontend = async () => {
 // curl -X POST -H "Content-Type: application/json" -d '{"accountType":"substrate","message":"i am testing", "signature":"0xb204163ca47ff05d2e8b1accb858acc10c06f6e694976cad2b5056221da67663eaaab8879b3d991cef3dab523fd7c5dc1071ad196d4351c2dab78d7f339b1b8f", "userAddress":"5Gv6J2jJSG7ZqrjKj22TsW2JgLqY5hL8oqDhBgRAcvmCxaKW"}' http://34.83.125.53:1985/verifyUserSignature
 
 // curl -X POST -H "Content-Type: application/json" -d '{"accountType":"substrate","message":"aiweb3", "signature":"0xb2808f1a866ed5d4a80886af874d8f1de0efd4cfa6e74e6affe7e36e08cc1b107e5de13606b537cfae9c8e8f09458560db9e754a807780dec2328d4dc5f66b81", "userAddress":"5Gazt49AnPMPRNt4U2mdJydoi9EKxZNFvJ85U7v1PyJzPmY4"}' http://127.0.0.1:1985/verifyUserSignature
+// curl -X POST -H "Content-Type: application/json" -d '{"userAddress":"0x345fdA96178147bF5E8cdFbfBDF723d15f2973C3","feature":{"feature1":"red hair","feature2":"red hat"}}' http://127.0.0.1:1985/generateTaskFromFrontend
+
