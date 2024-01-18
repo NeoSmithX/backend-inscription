626c5b1990ff5209e52a88974bfbd458e09f3766
Neo <NeoSmith1999@protonmail.com>
2024-01-18 12:45:11 +0800

    init project

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const run_relay_1 = require("./my_fn/run_relay");
-async function main() {
-    (0, run_relay_1.run_relay)();
-}
-main().then(() => {
-});
-//# sourceMappingURL=index.js.map
--- src/config/general.ts
+++ src/config/general.ts
@@ -1,22 +1,9 @@
-type ConfigRelay = {
-    ai:{
-        url:string,
-        port:number
-    },
-    frontend:{
-        url:string,
-        port:number
-    }
-
+type NetworkConfig = {
+    url:string,
+    port:number,
 }
 
-export const configRelay:ConfigRelay = {
-    'ai':{
-        url:'http://127.0.0.1',
-        port:1984,
-    },
-    'frontend':{
-        url:'http://127.0.0.1',
-        port:1985,
-    }
+export const network_config:NetworkConfig = {
+    url:'http://127.0.0.1',
+    port:1986,
 }
